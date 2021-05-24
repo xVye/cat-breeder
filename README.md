4a90b46a2cb65fca2a77c796278bed4ba259bd35
xVye <erikhampus@live.se>
2021-05-24 19:40:51 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Example
-An example greeter plugin
+# Cat Breeder ![logo](icon.png)
+#### Detailed information for raising kittens
--- src/test/java/net/pixxie/ExamplePluginTest.java
+++ src/test/java/net/pixxie/ExamplePluginTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package net.pixxie;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
