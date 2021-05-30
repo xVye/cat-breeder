4e9423230f2b219cdb05a1ea7bd3a80d9a1ac537
xVye <erikhampus@live.se>
2021-05-30 10:40:22 +0200

    Added copyright snippet

--- src/main/java/com/kittentimer/KittenActivityTimer.java
+++ src/main/java/com/kittentimer/KittenActivityTimer.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2021, xVye
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.kittentimer;
 
 import lombok.Getter;
@@ -13,33 +37,33 @@ import java.time.temporal.ChronoUnit;
 
 public class KittenActivityTimer extends Timer
 {
-    @Getter
-    @Setter
-    private boolean visible;
+	@Getter
+	@Setter
+	private boolean visible;
 
-    public KittenActivityTimer(Duration duration, BufferedImage image, Plugin plugin, boolean visible)
-    {
-        super(duration.toMillis(), ChronoUnit.MILLIS, image, plugin);
-        setTooltip("Time until your kitten needs attention or food");
-        this.visible = visible;
-    }
+	public KittenActivityTimer(Duration duration, BufferedImage image, Plugin plugin, boolean visible)
+	{
+		super(duration.toMillis(), ChronoUnit.MILLIS, image, plugin);
+		setTooltip("Time until your kitten needs attention or food");
+		this.visible = visible;
+	}
 
-    @Override
-    public Color getTextColor()
-    {
-        Duration timeLeft = Duration.between(Instant.now(), getEndTime());
+	@Override
+	public Color getTextColor()
+	{
+		Duration timeLeft = Duration.between(Instant.now(), getEndTime());
 
-        if (timeLeft.getSeconds() < Kitten.ATTENTION_WARN_TIME)
-        {
-            // TODO: Send notification if enabled
-            return Color.RED.brighter();
-        }
-        return Color.WHITE;
-    }
+		if (timeLeft.getSeconds() < Kitten.ATTENTION_WARN_TIME)
+		{
+			// TODO: Send notification if enabled
+			return Color.RED.brighter();
+		}
+		return Color.WHITE;
+	}
 
-    @Override
-    public boolean render()
-    {
-        return visible && super.render();
-    }
+	@Override
+	public boolean render()
+	{
+		return visible && super.render();
+	}
 }
